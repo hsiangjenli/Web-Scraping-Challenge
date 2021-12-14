f5af22b272ae61db9d94c29ab2a23e862e5e10af
hsiangjenli <hsiangjenli@gmail.com>
2021-12-15 01:32:05 +0800

    Create transformDate.py

---
+++ TWSE｜台灣證券交易所/TWSE/transformDate.py
@@ -0,0 +1,23 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+class TDate:
+
+    class str:
+        def toDate(string):
+            return datetime.datetime.strptime(string, '%Y-%m-%d')
+        def withNospace(string):
+            return TDate.date.toStringNospace(TDate.str.toDate(string))
+	
+    class plus:
+        def days(date, delta = 1):
+            return date + datetime.timedelta(days = delta)
+        def months(date, delta = 1):
+            return date + relativedelta(months = delta)
+
+    class date:
+        def toStringwithSlash(date):
+            return f'{date.year}/{date.month}/{date.day}'
+
+        def toStringNospace(date):
+            return date.strftime('%Y%m%d')
